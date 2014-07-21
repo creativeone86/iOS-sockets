64c5431704c7fb2fe329dfc3c011f27116db7edd
Yordan Georgiev <iordan340@gmail.com>
2014-07-21 16:29:37 +0300

    First commit
    Workin socket

---
+++ socketIOexample/socketIOexample/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  socketIOexample
+//
+//  Created by Htain Lin Shwe on 21/10/13.
+//  Copyright (c) 2013 saturngod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SocketIO.h"
+@interface ViewController : UIViewController <SocketIODelegate>
+@property (nonatomic,strong) SocketIO* socketIO;
+
+@property (strong, nonatomic) IBOutlet UITextView *chatLog;
+
+@property (nonatomic,assign) IBOutlet UITextField* chatbox;
+
+-(IBAction)sendMsg:(id)sender;
+@end
---
+++ sockio-server/app.js
@@ -0,0 +1,54 @@
+//require necessary modules
+var http = require('http')
+  , express = require('express')
+  , socketIO = require("socket.io")
+  , path = require('path');
+
+//initialize our application
+var app = express();
+app.use(express.static(path.join(__dirname, 'assets')));
+var server = http.createServer(app).listen(3000);
+var io = socketIO.listen(server);
+
+//settings
+var settings = {
+  'view_directory': '/views'
+}
+
+
+app.get('/', function(request, response){
+  response.sendfile(__dirname + settings.view_directory + '/index.html')
+});
+ 
+
+//chat using socket.io
+io.sockets.on('connection', function(client){
+  console.log("im in!");
+
+  client.on("getUser", function(data){
+    client.broadcast.emit('renderInHtml',data);
+    //client.emit("renderInHtml", data);
+    console.log("data from ios");
+    console.log(data.age);
+
+  });
+  //when client sends a join event
+  client.on('join', function(data){
+    client.set('nickname', data);
+    client.broadcast.emit('message', { message: data + " just joined!", nickname: "Server Announcement" });
+  });
+
+  //when client sends a message
+  client.on('message', function(data){
+    client.get('nickname', function(err, nickname){
+      client.broadcast.emit('message', { message: data, nickname: nickname });      
+    });
+  });
+
+  client.on("disconnect", function(data){
+
+    console.log("the user is disconnected!");
+
+  });
+
+})
